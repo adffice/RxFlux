1433297e9b230e3fb50f7e92624101269e64dbcd
adffice <adffice@gmail.com>
2016-05-23 15:12:30 +0800

    update unregisterRxView before registerRxView in BaseFragment

--- app/src/main/java/com/hardsoftstudio/rxflux/sample/ui/BaseFragment.java
+++ app/src/main/java/com/hardsoftstudio/rxflux/sample/ui/BaseFragment.java
@@ -2,7 +2,6 @@ package com.hardsoftstudio.rxflux.sample.ui;
 
 import android.app.Fragment;
 import android.os.Bundle;
-
 import com.hardsoftstudio.rxflux.dispatcher.RxViewDispatch;
 import com.hardsoftstudio.rxflux.sample.SampleApp;
 import com.hardsoftstudio.rxflux.store.RxStoresRegister;
@@ -11,34 +10,30 @@ import com.hardsoftstudio.rxflux.store.RxStoresRegister;
  * Created by dallas on 16-5-16.
  */
 public abstract class BaseFragment extends Fragment implements RxViewDispatch, RxStoresRegister {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
+  @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     onRxStoresRegister();
   }
 
-  @Override
-  public void onActivityCreated(Bundle savedInstanceState) {
+  @Override public void onActivityCreated(Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
 
+    SampleApp.get(getActivity()).getRxFlux().getDispatcher().unregisterRxView(this);
     SampleApp.get(getActivity()).getRxFlux().getDispatcher().registerRxView(this);
   }
 
-  @Override
-  public void onResume() {
+  @Override public void onResume() {
     super.onResume();
     SampleApp.get(getActivity()).getRxFlux().getDispatcher().registerRxView(this);
   }
 
-  @Override
-  public void onStop() {
+  @Override public void onStop() {
     super.onStop();
     SampleApp.get(getActivity()).getRxFlux().getDispatcher().unregisterRxView(this);
   }
 
-  @Override
-  public void onDestroy() {
+  @Override public void onDestroy() {
     super.onDestroy();
     onRxStoresUnRegister();
   }
